181d2235e32fc1660edd78dd0a2681a69d45dc06
J2TeaM <junookyo@gmail.com>
2016-11-08 08:04:31 +0700

    Format code

--- config.php
+++ config.php
@@ -1,19 +1,18 @@
 <?php
+defined('BASEPATH') OR exit('Access denied.');
 
 $logfile = '/var/log/nginx/access.log';
 
-$serverip = '125.212.250.72';
-$yourip = '1.53.194.96';
-$exclude_ips = array($serverip,
-					$yourip,
-
-					'1.53.194.96',
-
-				);
+$serverip    = '125.212.250.72';
+$yourip      = '1.53.194.96';
+$exclude_ips = array(
+  $serverip,
+  $yourip,
+  '1.53.194.96'
+);
 
 $timewindow = 60; //unit: second - a splited interval time that requests are grouped in. For example one minute have 5000 requests
-$threshold = 1000; //threshold requests number per $timewindow
-
+$threshold  = 1000; //threshold requests number per $timewindow
 $sleeptime = 10; //unit: second - sleep between log check
 
 ?>
